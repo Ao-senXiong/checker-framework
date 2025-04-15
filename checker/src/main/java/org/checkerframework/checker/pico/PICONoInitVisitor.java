93807d37d2cd3d3b6f65c6ed7ae2cafa44662895
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-04-15 15:55:59 -0400

    Update default

--- checker/src/main/java/org/checkerframework/checker/pico/PICONoInitVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/pico/PICONoInitVisitor.java
@@ -87,7 +87,8 @@ public class PICONoInitVisitor extends BaseTypeVisitor<PICONoInitAnnotatedTypeFa
             AnnotatedDeclaredType declarationType, AnnotatedDeclaredType useType, Tree tree) {
 
         // FIXME workaround for poly anno, remove after fix substitutable poly and add poly vp rules
-        if (useType.hasAnnotation(atypeFactory.POLY_MUTABLE)) {
+        if (useType.hasAnnotation(atypeFactory.POLY_MUTABLE)
+                || useType.hasAnnotation(atypeFactory.LOST)) {
             return true;
         }
 
